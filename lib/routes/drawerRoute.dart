e07386e00572b8510b564e39d6723fbce6bae2cd
Wong Kai Wen <e0544055@u.nus.edu>
2021-05-31 16:59:12 +0800

    WIP janky drawer

--- lib/routes/drawerRoute.dart
+++ lib/routes/drawerRoute.dart
@@ -1,9 +1,10 @@
 import 'package:orbital_app/screens/drawer/profilepage.dart';
-import 'package:orbital_app/screens/home/home.dart';
+// import 'package:orbital_app/screens/home/home.dart';
+import 'package:orbital_app/screens/wrapper.dart';
 
 
 class drawerRoutes {
- static const String home = Home.routeName;
+ // static const String home = Home.routeName;
  static const String profilePage = ProfilePage.routeName;
-
+ static const String wrapper = Wrapper.routeName;
 }
