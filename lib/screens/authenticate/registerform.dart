1e06a195306ece675f6c8f9cfb6fb06a8bc9f0d9
yunpeng1234 <xiao.yunpeng99@gmail.com>
2021-06-04 20:33:25 +0800

    some small edits

--- lib/screens/authenticate/registerform.dart
+++ lib/screens/authenticate/registerform.dart
@@ -19,7 +19,7 @@ class _RegisterFormState extends State<RegisterForm> {
 
   final AuthService _auth = AuthService();
   final _formkey = GlobalKey<FormState>();
-   bool loading = false;
+  bool loading = false;
 
   //text field state
   String email = '';
@@ -72,80 +72,7 @@ class _RegisterFormState extends State<RegisterForm> {
           }
         },
         onSignInTapped: () => widget.toggler(),
-        // onBackPressed: () => widget.toggler(),
       ),
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return loading ? Loading() : Scaffold(
-  //     backgroundColor: Color(0xffFCE5CD),
-  //     appBar: AppBar(
-  //       backgroundColor: Colors.brown[200],
-  //       elevation: 0.0,
-  //       title: Text('Sign Up'),
-  //       actions: <Widget>[
-  //         TextButton.icon(
-  //           icon: Icon(Icons.person),
-  //           label: Text('Sign In'),
-  //           onPressed: () {
-  //             widget.toggler();
-  //           },
-  //           )
-  //         ],
-  //       ),
-  //     body: Container(
-  //       padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-  //       child: Form(
-  //         key: _formkey,
-  //         child : Column(
-  //           children: <Widget>[
-  //             SizedBox(height: 20.0),
-  //             TextFormField(
-  //               decoration: textBoxDeco.copyWith(hintText: "Email"),
-  //               validator: (val) => val.isEmpty ? "Enter an email" : null,
-  //               onChanged: (val) {
-  //                 setState(() => email = val.trim());
-  //               }
-  //             ),
-  //             SizedBox(height: 20.0),
-  //             TextFormField(
-  //               decoration: textBoxDeco.copyWith(hintText: "Password"),
-  //               validator: (val) => val.length < 6 ? "Please enter a password of at least 6 characters" : null,
-  //               obscureText: true,
-  //               onChanged: (val) {
-  //                 setState(() => password = val);
-  //               }
-  //             ),
-  //             SizedBox(height: 20.0),
-  //             ElevatedButton(
-  //               child: Text(
-  //                 'Register',
-  //                 style: TextStyle(color: Colors.white),
-  //               ),
-  //               onPressed: () async {
-  //                 if (_formkey.currentState.validate()) {
-  //                   setState(() => loading = true);
-  //                   dynamic result = await _auth.registerNative(email.trim(), password);
-  //                   if (result == null) {
-  //                     setState(() {
-  //                       error = "Please provide a valid email address";
-  //                       loading = false;
-  //                       });
-  //                   }
-  //                 }
-  //               }
-  //             ),
-  //             SizedBox(height: 12.0),
-  //             Text(
-  //               error,
-  //               style: TextStyle(color: Colors.red, fontSize: 14.0)
-  //             ),
-  //           ],
-  //           )
-  //       )
-  //     ),
-  //   );
-  // }
 }
