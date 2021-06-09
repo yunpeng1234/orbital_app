3ed1709703ffff4b8da4b56ca0c1cffa5d6aba94
yunpeng1234 <xiao.yunpeng99@gmail.com>
2021-06-09 17:23:24 +0800

    changed profilepage

--- lib/screens/drawer/profilepage.dart
+++ lib/screens/drawer/profilepage.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:orbital_app/models/user.dart';
-import 'package:orbital_app/screens/drawer/profilepageinner.dart';
 import 'package:orbital_app/shared/app_drawer.dart';
 import "package:provider/provider.dart";
 import "package:orbital_app/services/database.dart";
 import "package:cloud_firestore/cloud_firestore.dart";
+import 'package:orbital_app/shared/constants.dart';
 
 class ProfilePageView extends StatefulWidget {
   static const String routeName = '/profile';
@@ -14,19 +14,49 @@ class ProfilePageView extends StatefulWidget {
 }
 
 class _ProfilePageViewState extends State<ProfilePageView> {
+  final string = FirebaseAuth.instance.currentUser.uid;
+
+  String toUpdate = '';
+  final DatabaseService user  = DatabaseService(uid: FirebaseAuth.instance.currentUser.uid);
+
   @override
   Widget build(BuildContext context) {
-    final string = FirebaseAuth.instance.currentUser.uid;
-    return StreamProvider<IndividualData>.value (
-      value: DatabaseService(uid: string).userData,
-      initialData: IndividualData(),
-      child: Scaffold(
-       appBar: AppBar(
-         title: Text("My Profile"),
-       ),
-       drawer: AppDrawer(),
-       body: UserDataDisplay(),
-       )
-    );
+    return StreamBuilder(
+      stream: user.userData,
+      builder:(BuildContext context, AsyncSnapshot<IndividualData> snapshot) {
+        if(!snapshot.hasData) return new Text('Load');
+        String username = snapshot.data.name;
+        return Scaffold(
+            appBar: AppBar(title: Text('texting'),),
+            drawer: AppDrawer(),
+            backgroundColor: primaryColor,
+            resizeToAvoidBottomInset: false,
+            body: Column(
+              children: <Widget>[
+                Text(username, style: TextStyle(fontSize: 50.0)),
+                SizedBox(height: 10.0),
+                Form(
+                  child: TextFormField(
+                    decoration: textBoxDeco.copyWith(hintText: "New Username"),
+                    onChanged: (val) {
+                      setState(() {
+                        toUpdate = val;
+                      });
+                    },
+                  )
+                ),
+                TextButton(
+                  onPressed: () async {
+                    user.updateUserData(toUpdate);
+                  },
+                  child: Text("Submit"),
+                  ),
+              ],
+            )
+        );
+      
+      }
+      
+      );
  }
 }
