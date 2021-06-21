9c9557de7168cb4e379e03d145541fce39d3669e
yunpeng1234 <xiao.yunpeng99@gmail.com>
2021-06-21 18:12:28 +0800

    SearchBar

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:orbital_app/shared/app_drawer.dart';
 import 'package:orbital_app/shared/constants.dart';
 import 'package:orbital_app/shared/loading.dart';
@@ -27,15 +28,13 @@ class HomeView extends StatelessWidget {
       builder: (context, model, child) => model.state == ViewState.busy
         ? Center(child: Loading())
         : Scaffold(
-          appBar: AppBar(
-          ),
             drawer: AppDrawer(),
             backgroundColor: primaryColor,
             resizeToAvoidBottomInset: false,
             body: Stack(
               fit: StackFit.expand,
               children: <Widget>[
-                // buildFloatingSearchBar(),
+                
                 Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: ListView(
@@ -120,7 +119,8 @@ class HomeView extends StatelessWidget {
                     
                     ],
                   ),
-                )
+                ),
+                buildFloatingSearchBar(),
               ],
             )
           ),
