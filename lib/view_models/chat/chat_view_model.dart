719d7fe6bce31dcea859ff02479255c670290691
yunpeng1234 <xiao.yunpeng99@gmail.com>
2021-07-03 12:18:51 +0800

    Minor Tweaks

--- lib/view_models/chat/chat_view_model.dart
+++ lib/view_models/chat/chat_view_model.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:orbital_app/models/message.dart';
-import 'package:orbital_app/screens/chat/chat_view.dart';
 import 'package:orbital_app/services/auth_service.dart';
-import 'package:orbital_app/services/database.dart';
-
 import '../base_view_model.dart';
-import 'package:orbital_app/models/contact.dart';
 import 'package:orbital_app/services/message_service.dart';
 import 'package:orbital_app/services/service_locator.dart';
 
